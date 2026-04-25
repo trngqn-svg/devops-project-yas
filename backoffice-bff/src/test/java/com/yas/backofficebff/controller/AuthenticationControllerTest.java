db10b12fb15a8a8cda1aa19d1cd940091f713717
trngqn-svg <trantrongquan062@gmail.com>
2026-04-25 15:17:12 +0700

    Update AuthenticationControllerTest.java

--- backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
@@ -3,7 +3,6 @@ package com.yas.backofficebff.controller;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
