04cf0f6589075db47f04c5e046f56dde1065850f
trngqn-svg <trantrongquan062@gmail.com>
2026-04-25 17:16:29 +0700

    Update DeliveryControllerTest.java

--- delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
+++ delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
@@ -1,26 +1,26 @@
 package com.yas.delivery.controller;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import com.yas.delivery.service.DeliveryService;
 
-@WebMvcTest(DeliveryController.class)
 class DeliveryControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private final DeliveryService deliveryService = mock(DeliveryService.class);
 
-    @MockBean
-    private DeliveryService deliveryService;
+    private final DeliveryController deliveryController =
+            new DeliveryController(deliveryService);
+
+    private final MockMvc mockMvc =
+            MockMvcBuilders.standaloneSetup(deliveryController).build();
 
     @Test
     void testDeliveryControllerInitialization() {
         assertNotNull(mockMvc);
     }
-
 }
