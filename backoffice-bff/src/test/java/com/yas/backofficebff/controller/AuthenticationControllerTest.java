6803e7c731fa72833977dc90fe898a6fcebded7f
trngqn-svg <trantrongquan062@gmail.com>
2026-04-25 11:46:39 +0700

    Update AuthenticationControllerTest.java

--- backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
@@ -2,20 +2,17 @@ package com.yas.backofficebff.controller;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.mockito.Mockito;
 
 @WebMvcTest(AuthenticationController.class)
-@AutoConfigureMockMvc(addFilters = false)
 class AuthenticationControllerTest {
 
     @Autowired
@@ -27,7 +24,9 @@ class AuthenticationControllerTest {
         when(principal.getAttribute("preferred_username")).thenReturn("admin");
 
         mockMvc.perform(get("/authentication/user")
-                .with(SecurityMockMvcRequestPostProcessors.oauth2Login().oauth2User(principal)))
+                .with(SecurityMockMvcRequestPostProcessors
+                        .oauth2Login()
+                        .oauth2User(principal)))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.username").value("admin"));
     }
