8d53e03e5b3184febdd92fbbf76a8685f7afb2b5
trngqn-svg <trantrongquan062@gmail.com>
2026-04-25 17:19:56 +0700

    Update DeliveryControllerTest.java

--- delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
+++ delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
@@ -1,20 +1,15 @@
 package com.yas.delivery.controller;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.yas.delivery.service.DeliveryService;
-
 class DeliveryControllerTest {
 
-    private final DeliveryService deliveryService = mock(DeliveryService.class);
-
     private final DeliveryController deliveryController =
-            new DeliveryController(deliveryService);
+            new DeliveryController();
 
     private final MockMvc mockMvc =
             MockMvcBuilders.standaloneSetup(deliveryController).build();
