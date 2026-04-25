eca9eba2090ab3a660dd8c6bd0812e1175d493c5
trngqn-svg <trantrongquan062@gmail.com>
2026-04-25 11:55:29 +0700

    Update AuthenticationControllerTest.java

--- backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
@@ -1,33 +1,30 @@
 package com.yas.backofficebff.controller;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@WebMvcTest(AuthenticationController.class)
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(controllers = AuthenticationController.class)
 class AuthenticationControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @Test
-    void testUser_whenAuthenticated_returnsUser() throws Exception {
+    void testUser_whenAuthenticated_returnsUser() {
         OAuth2User principal = Mockito.mock(OAuth2User.class);
         when(principal.getAttribute("preferred_username")).thenReturn("admin");
 
-        mockMvc.perform(get("/authentication/user")
-                .with(SecurityMockMvcRequestPostProcessors
-                        .oauth2Login()
-                        .oauth2User(principal)))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.username").value("admin"));
+        webTestClient.get()
+                .uri("/authentication/user")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.username").isEqualTo("admin");
     }
 }
